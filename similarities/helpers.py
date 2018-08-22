310022fe5232cceb7410195af488a8e650f5aa84
Carina Cruz <lecruz@bu.edu>
2018-08-22 01:05:17 +0000

    update

--- similarities/helpers.py
+++ similarities/helpers.py
@@ -1,7 +1,13 @@
 #3 functions in this file: algorithms for lines, sentences, and substrings
 
+# Implement lines in such a way that, given two strings, a and b, it returns a list of the lines that are, identically, in both a and b.
+# The list should not contain any duplicates. Assume that lines in a and b will be be separated by \n,
+# but the strings in the returned list should not end in \n. If both a and b contain
+# one or more blank lines (i.e., a \n immediately preceded by no other characters), the returned list should include an empty string (i.e., "").
+
 def lines(a, b):
     """Return lines in both a and b"""
+
 
     # TODO
     return []
