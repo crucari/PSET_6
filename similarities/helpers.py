3c2a17729e82f717142e371b588862d6fa268ea9
Carina Cruz <lecruz@bu.edu>
2018-08-23 18:30:17 +0000

    sentences

--- similarities/helpers.py
+++ similarities/helpers.py
@@ -12,17 +12,10 @@ def lines(a, b):
 
     return list(set(a.split('\n')).intersection(b.split('\n')))
 
-    # # TODO
-    # return []
-
-
-
 def sentences(a, b):
     """Return sentences in both a and b"""
 
-    # TODO
-    return []
-
+    return list(set(sent_tokenize(a)).intersection(sent_tokenize(b)))
 
 def substrings(a, b, n):
     """Return substrings of length n in both a and b"""
